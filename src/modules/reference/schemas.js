9065e36689ef35917cb805f568da08e169aefabd
jamisonholt <jamisonholt95@gmail.com>
2017-04-22 11:38:58 -0500

    updoot

--- src/modules/reference/schemas.js
+++ src/modules/reference/schemas.js
@@ -4,7 +4,21 @@ var UserSchema = new mongoose.Schema({
   username: String,
   password: String,
   admin: Boolean,
+  number: String,
+  picture: String,
+  location: String,
+  rating: Number,
+  subscriptions: String,
 })
 UserSchema.index({username: 'text'});
+var Card = new mongoose.Schema({
+	item: String,
+	user: String,
+	number: String,
+	location: String,
+	rating: Number,
+})
+Card.index({item: 'text', user: 'text'})
 
-export { UserSchema };
+
+export { UserSchema, Card };
